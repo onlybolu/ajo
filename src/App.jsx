9ee00385604522aac393fe3b89bafa4b9aa4f3a6
Shakaaraahagency <boluwatifeobateru@gmail.com>
2025-02-14 10:09:22 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router,Route, Routes } from 'react-router-dom'
 import Home from './component/pages/Home'
-import Login from './component/Authentication/login'
+// import Login from './component/Authentication/login'
 import Signup from './component/Authentication/Signup'
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/login" element={<Login/>} />
+        {/* <Route exact path="/login" element={<Login/>} /> */}
         <Route exact path="/signup" element={<Signup />} />
       </Routes>
     </Router>
