8406e892469c29bcf06962742de2f1ad08bc3f5b
boluwatife obateru <boluwatifeobateru@gmail.com>
2025-04-13 23:31:30 -0700

    Update Slip.jsx

--- src/component/pages/Slip.jsx
+++ src/component/pages/Slip.jsx
@@ -8,7 +8,7 @@ const Slip = () => {
             <h1 className='text-center text-md-start'  style={{fontSize:"40px",fontFamily:"Urbanist, serif"}}>Safe, secure and fast.</h1>
             <p className="text-wrap w-full md:w-1/2 text-md-wrap">Receive money into your wallets on Realsavers, get to make transactions with the money and also withdraw it to your bank account or send to third-parties.</p>
             </div><br/>
-            <div className="containerd d-flex px-4 px-md-0 flex-column flex-md-row  gap-10">
+            <div className="container flex px-4 px-md-0 flex-col md:flex-row  gap-10">
                 <div className="" style={{background:"#f5f5f5",padding:"40px 20px",borderRadius:"30px"}}>
                     <div className="card-body">
                         <img src="/naira-search.svg" alt="" /><br/>
@@ -31,7 +31,7 @@ const Slip = () => {
                     </div>
                 </div>
             </div><br/><br/>
-            <div className="d-block d-md-flex gap-10 mb-20">
+            <div className="d-block d-md-flex gap-10 px-3 md:px-0 mb-20">
                 <div className="w-full md:w-1/2">
                     <img src="/testimonial-1.png" className="img-fluid " alt="" />
                 </div>
