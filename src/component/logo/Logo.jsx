927cb7d21dfb8bdfb2187c915caac708aca204b1
boluwatife obateru <boluwatifeobateru@gmail.com>
2025-04-17 11:10:50 -0700

    Update Logo.jsx

--- src/component/logo/Logo.jsx
+++ src/component/logo/Logo.jsx
@@ -3,7 +3,7 @@
 const Logo = () => {
   return (
     <div>
-         <div className='text-center items-center justify-center flex w-30 rounded-pill'  onClick={() => window.location.href="/"}>
+         <div className='text-center flex w-30 rounded-pill' style={{paddingLeft:"20px",cursor:"pointer",paddingTop:"10px", paddingBottom:"10px",background: "linear-gradient(#e3e5e6,#ece6cb,#e4e4e2,#eee8cd)",width:"100%"}} onClick={() => window.location.href="/"}>
         <img className='img-fluid w-10 h-10' src="./logo.png" alt="" />
          <p className='pt-2 '>Realsavers</p>
         </div>
